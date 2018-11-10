daa5fcc977516e686e11caf9fe7189d783774cd8
Michael St Onge <mcstonge@asu.edu>
2018-11-10 10:21:51 -0700

    Mikey's Branch

--- Backend/JavaScripts/StockHacks/src/backTesting/Algo1.java
+++ Backend/JavaScripts/StockHacks/src/backTesting/Algo1.java
@@ -1,12 +1,19 @@
 package backTesting;
 
-import org.json.simple.JSONObject;
+import org.json.*;
+import java.net.*;
+import java.io.*;
 
 public class Algo1 {
 
 	public static void main(String[] args) 
 	{
 		JSONObject obj = new JSONObject();
+		
+		//URL url = new URL("http://example.com");
+		//HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		//con.setRequestMethod("GET");
+		
 		System.out.println("Hi");
 	}
 
