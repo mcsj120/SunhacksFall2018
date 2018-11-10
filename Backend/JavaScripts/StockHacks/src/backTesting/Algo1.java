0f1a07777073a1ec693826760ae76ba9cd5e0dac
Pratik Panda <ppanda.soccerrules@gmail.com>
2018-11-10 00:07:31 -0700

    Added JSON Simple

--- Backend/JavaScripts/StockHacks/src/backTesting/Algo1.java
+++ Backend/JavaScripts/StockHacks/src/backTesting/Algo1.java
@@ -1,9 +1,12 @@
 package backTesting;
 
+import org.json.simple.JSONObject;
+
 public class Algo1 {
 
 	public static void main(String[] args) 
 	{
+		JSONObject obj = new JSONObject();
 		System.out.println("Hi");
 	}
 
